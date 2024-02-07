0293510c0dab4ca4000f30c6aa159ea5ea9421a3
unknown <alysonantunes@gmail.com>
2024-02-07 19:55:54 +0100

    Aula-2 Concluida

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,23 +1,43 @@
-import { View, Text, FlatList } from 'react-native'
+import { View, Text, FlatList, SectionList } from 'react-native'
 
-import { CATEGORIES } from "@/utils/data/products"
+import { CATEGORIES, MENU } from "@/utils/data/products"
+import { Product } from '@/components/product'
 
+import React, { useState, useRef } from 'react'
 import { Header } from '@/components/header'
 import { CategoryButton } from '@/components/category-button'
-import { useState } from 'react'
+import { Link } from 'expo-router'
+import { useCartStore } from '@/stores/cart-store'
 
 export default function Home() {
+  const cartStore = useCartStore()
   const [category, setCategory] = useState(CATEGORIES[0])
 
+  const sectionListRef = useRef<SectionList>(null)
+
+  const cartQuantityItems = cartStore.products.reduce((total, product) => total + product.quantity, 0)
 
   function handleCagetorySelect(selectedCategory: string) {
     setCategory(selectedCategory)
+
+    const sectionIndex = CATEGORIES.findIndex((category) => category === selectedCategory)
+
+    console.log(sectionIndex)
+
+    if (sectionListRef.current) {
+      sectionListRef.current.scrollToLocation({
+        animated: true,
+        sectionIndex,
+        itemIndex: 0,
+      })
+    }
   }
+
 
   return (
     <View className="flex-1 pt-8">
 
-      <Header title="Faça seu Pedido" cartQuantityItems={5} />
+      <Header title="Faça seu Pedido" cartQuantityItems={cartQuantityItems} />
       {/* <Text className="text-white text-2xl text-center font-heading" >Bienvenido al Chuin Menu😻</Text> */}
 
       <FlatList
@@ -36,8 +56,27 @@ export default function Home() {
         showsHorizontalScrollIndicator={false}
       />
 
+      <SectionList
+        ref={sectionListRef}
+        sections={MENU}
+        keyExtractor={(item) => item.id}
+        stickySectionHeadersEnabled={false}
+
+        renderItem={({ item }) => (
+          <Link href={`../product/${item.id}`} asChild>
+            <Product data={item} />
+          </Link>
+        )}
+
+        renderSectionHeader={({ section: { title } }) => (
+          <Text className='text-xl text-white font-heading mt-8 mb-3'>{title}</Text>
+        )}
+        className='flex-1 p-5'
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100, }}
+      />
+
     </View>
 
   )
 }
-
--- src/components/category-button.tsx
+++ src/components/category-button.tsx
@@ -12,6 +12,6 @@ export function CategoryButton({ title, isSelected, ...rest }: CategoryProps) {
     <Pressable className={clsx('bg-slate-800 px-4 justify-center rounded-md h-10'
       , isSelected && "border-2 border-lime-300")} {...rest}>
       <Text className='text-slate-100 font-subtitle text-sm'>{title}</Text>
-    </Pressable >
+    </Pressable>
   )
 }
